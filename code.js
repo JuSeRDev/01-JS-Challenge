b6cc22cfb2af1297e62e4879993c2608da1d1a25
JuSeRDev <juserdev@gmail.com>
2024-09-30 07:31:56 -0500

    Modificacion de JS

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ const paragraph = document.querySelectorAll(".paragraph")
 const plus = document.querySelectorAll(".plus")
 const minus = document.querySelectorAll(".minus")
 
-title.forEach((subtitle, i)=>{
+/* title.forEach((subtitle, i)=>{
     title[i].addEventListener("click", ()=>{
         if (paragraph[i].style.display === "none") {
             paragraph[i].style.display = "flex"
@@ -15,5 +15,20 @@ title.forEach((subtitle, i)=>{
             plus[i].style.display = "flex"
             minus[i].style.display = "none"
         }
+    })
+}) */
+
+title.forEach((subtitle, i)=>{
+    subtitle.addEventListener("click", ()=>{
+        paragraph[i].classList.toggle("open")
+            if (paragraph[i].classList.contains("open")) {
+                plus[i].style.display = "none"
+                minus[i].style.display = "block"
+                paragraph[i].style.padding = "30px 0 20px 0"
+            } else {
+                plus[i].style.display = "block"
+                minus[i].style.display = "none"
+                paragraph[i].style.padding = ""
+            }
     })
 })
